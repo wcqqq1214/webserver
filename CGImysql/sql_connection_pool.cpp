77bad1d3efe6d7524f1ee38ebbf0f940c29de776
weichengqian <wcqqq1214@gmail.com>
2025-07-08 11:27:40 +0800

    feat: 完成数据库连接池

---
+++ CGImysql/sql_connection_pool.cpp
@@ -0,0 +1,122 @@
+#include "sql_connection_pool.h"
+
+connection_pool::connection_pool() {
+    m_CurConn = 0;
+    m_FreeConn = 0;
+}
+
+connection_pool* connection_pool::GetInstance() {
+    static connection_pool connPool;
+    return &connPool;
+}
+
+// 构造初始化
+void connection_pool::init(string url, string User, string Password, string DatabaseName, int Port, int MaxConn, int close_log) {
+    m_url = url;
+    m_Port = Port;
+    m_User = User;
+    m_Password = Password;
+    m_DatabaseName = DatabaseName;
+    m_close_log = close_log;
+
+    for (int i = 0; i < MaxConn; i++) {
+        MYSQL* conn = NULL;
+        conn = mysql_init(conn);
+
+        if (conn == NULL) {
+            LOG_ERROR("MySQL Error");
+            exit(1);
+        }
+        conn = mysql_real_connect(conn, url.c_str(), User.c_str(), Password.c_str(), DatabaseName.c_str(), Port, NULL, 0);
+
+        if (conn == NULL) {
+            LOG_ERROR("MySQL Error");
+            exit(1);
+        }
+
+        connList.push_back(conn);
+        ++m_FreeConn;
+    }
+
+    reserve = sem(m_FreeConn);
+
+    m_MaxConn = m_FreeConn;
+}
+
+// 当有请求时，从数据库连接池中返回一个可用连接，更新使用和空闲连接数
+MYSQL* connection_pool::GetConnection() {
+    MYSQL* conn = NULL;
+
+    if (0 == connList.size()) {
+        return NULL;
+    }
+
+    reserve.wait();
+
+    lock.lock();
+
+    conn = connList.front();
+    connList.pop_front();
+
+    --m_FreeConn;
+    ++m_CurConn;
+
+    lock.unlock();
+    return conn;
+}
+
+// 释放当前使用的连接
+bool connection_pool::ReleaseConnection(MYSQL *conn) {
+    if (NULL == conn) {
+        return false;
+    }
+
+    lock.lock();
+
+    connList.push_back(conn);
+    
+    ++m_FreeConn;
+    --m_CurConn;
+    
+    lock.unlock();
+
+    reserve.post();
+    return true;
+}
+
+// 销毁数据库连接池
+void connection_pool::DestroyPool() {
+    lock.lock();
+    if (connList.size() > 0) {
+        list<MYSQL*>::iterator it;
+        for (it = connList.begin(); it != connList.end(); ++it) {
+            MYSQL* conn = *it;
+            mysql_close(conn);
+        }
+        m_CurConn = 0;
+        m_FreeConn = 0;
+        connList.clear();
+    }
+
+    lock.unlock();
+}
+
+// 获取当前空闲的连接数
+int connection_pool::GetFreeConn() {
+    return this->m_FreeConn;
+}
+
+connection_pool::~connection_pool() {
+    DestroyPool();
+}
+
+connectionRAII::connectionRAII(MYSQL** SQL, connection_pool* connPool) {
+    *SQL = connPool->GetConnection();
+
+    connRAII = *SQL;
+    poolRAII = connPool;
+}
+
+connectionRAII::~connectionRAII() {
+    poolRAII->ReleaseConnection(connRAII);
+}
---
+++ CGImysql/sql_connection_pool.h
@@ -0,0 +1,54 @@
+#ifndef _CONNECTION_POOL_
+#define _CONNECTION_POOL_
+
+#include <list>
+#include <mysql/mysql.h>
+#include <string>
+#include "../lock/locker.h"
+#include "../log/log.h"
+
+using namespace std;
+
+class connection_pool {
+public:
+    MYSQL* GetConnection();                 // 获取数据库连接
+    bool ReleaseConnection(MYSQL* conn);    // 释放连接
+    int GetFreeConn();                      // 获取当前空闲连接数
+    void DestroyPool();                     // 销毁所有连接
+
+    // 单例模式
+    static connection_pool* GetInstance();
+
+    void init(string url, string User, string Password, string DatabaseName, int Port, int MaxConn, int close_log);
+
+private:
+    connection_pool();
+    ~connection_pool();
+
+    int m_MaxConn;          // 最大连接数
+    int m_CurConn;          // 当前已使用的连接数
+    int m_FreeConn;         // 当前空闲的连接数
+    locker lock;
+    list<MYSQL*> connList;  // 连接池
+    sem reserve;
+
+public:
+    string m_url;           // 主机地址
+    string m_Port;          // 数据库端口号
+    string m_User;          // 登录数据库用户名
+    string m_Password;      // 登录数据库密码
+    string m_DatabaseName;  // 使用数据库名
+    int m_close_log;        // 日志开关
+};
+
+class connectionRAII {
+public:
+    connectionRAII(MYSQL** conn, connection_pool* connPool);
+    ~connectionRAII();
+
+private:
+    MYSQL* connRAII;
+    connection_pool* poolRAII;
+};
+
+#endif // !_CONNECTION_POOL_
