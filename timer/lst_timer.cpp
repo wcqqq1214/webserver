57104c122da27d0d1ef91f8ef03b942f0a150340
weichengqian <wcqqq1214@gmail.com>
2025-07-15 10:59:29 +0800

    fix: 为 lst_timer.cpp 引入 http_conn.h 头文件

--- timer/lst_timer.cpp
+++ timer/lst_timer.cpp
@@ -1,5 +1,5 @@
 #include "lst_timer.h"
-// #include "../http/http_conn.h"
+#include "../http/http_conn.h"
 
 sort_timer_lst::sort_timer_lst() {
     head = NULL;
@@ -202,5 +202,5 @@ void cb_func(client_data *user_data) {
     epoll_ctl(Utils::u_epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0);
     assert(user_data);
     close(user_data->sockfd);
-    // http_conn::m_user_count--;
+    http_conn::m_user_count--;
 }
